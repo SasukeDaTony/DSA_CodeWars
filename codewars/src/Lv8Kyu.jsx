2344cd30f26321fe776dd9c59fbfee14b2187ed7
SasukeDaTony <tonytaylor@pursuit.org>
2023-12-20 13:26:41 -0500

    function which quarter year DSA

--- codewars/src/Lv8Kyu.jsx
+++ codewars/src/Lv8Kyu.jsx
@@ -64,3 +64,28 @@ function past(h, m, s) {
 
   return hMilliseconds + mMilliseconds + sMilliseconds;
 }
+// /////////////////
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// Constraint:
+
+// 1 <= month <= 12
+
+const quarterOf = (month) => {
+  // Your code here
+  if (month < 4) {
+    return 1;
+  }
+  if (month > 3 && month < 7) {
+    return 2;
+  }
+
+  if (month > 6 && month < 10) {
+    return 3;
+  }
+
+  return 4;
+};
