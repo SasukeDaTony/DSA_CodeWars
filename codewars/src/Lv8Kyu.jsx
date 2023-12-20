0332b4544c7b1be4221d361b9ed079bff82989b2
SasukeDaTony <tonytaylor@pursuit.org>
2023-12-20 13:16:29 -0500

    function Milliseconds past midnight DSA

--- codewars/src/Lv8Kyu.jsx
+++ codewars/src/Lv8Kyu.jsx
@@ -38,3 +38,29 @@ function sameCase(a, b) {
 
   return 0;
 }
+//////////////////////
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+function past(h, m, s) {
+  //#Happy Coding! ^_^
+  const hMilliseconds = h * 60 * 60000;
+  const mMilliseconds = m * 60000;
+  const sMilliseconds = s * 1000;
+
+  return hMilliseconds + mMilliseconds + sMilliseconds;
+}
