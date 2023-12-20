4c89ffc8ee573396fbdbfd617bebb23ec0529624
SasukeDaTony <tonytaylor@pursuit.org>
2023-12-20 13:36:54 -0500

    alarm clock function DSA

--- codewars/src/Lv8Kyu.jsx
+++ codewars/src/Lv8Kyu.jsx
@@ -112,3 +112,22 @@ class Kata {
 function otherAngle(a, b) {
   return 180 - (a + b);
 }
+
+//   //////////////////
+
+// Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// employed | vacation
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+function setAlarm(employed, vacation) {
+  if (employed && !vacation) {
+    return true;
+  }
+  return false;
+}
